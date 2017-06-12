6be28ec57da975783a763145ad18544cd7b1049e
Guest User <Guest@Epicodus-6C.local>
2017-06-12 10:56:50 -0700

    add interface for item touch gestures

---
+++ app/src/main/java/com/example/guest/myrestaurants/util/ItemTouchHelperAdapter.java
@@ -0,0 +1,8 @@
+package com.example.guest.myrestaurants.util;
+
+public interface ItemTouchHelperAdapter {
+
+    boolean onItemMove(int fromPosition, int toPosition);
+
+    void onItemDismiss(int position);
+}
