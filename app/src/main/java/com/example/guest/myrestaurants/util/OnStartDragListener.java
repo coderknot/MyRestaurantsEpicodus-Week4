53b9d3ccc0a152c6e2ac3f3fd005c301cc8beb52
Guest User <Guest@Epicodus-6C.local>
2017-06-12 10:58:32 -0700

    add interface for item drag gestures

---
+++ app/src/main/java/com/example/guest/myrestaurants/util/OnStartDragListener.java
@@ -0,0 +1,8 @@
+package com.example.guest.myrestaurants.util;
+
+import android.support.v7.widget.RecyclerView;
+
+public interface OnStartDragListener {
+
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
